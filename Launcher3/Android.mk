3967635d353dee060528e68f2f2ea1bb6b117c59
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-09-22 10:20:18 +0800

    添加MK文件

---
+++ Launcher3/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+include $(BUILD_MULTI_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/../WallpaperPicker/res \
+    $(LOCAL_PATH)/src/main/res \
+	$(LOCAL_PATH)/../../../../../../prebuilts/sdk/current/support/v7/recyclerview/res
+LOCAL_JAVA_LIBRARIES := framework \
+                        telephony-common 
+LOCAL_STATIC_JAVA_LIBRARIES := 	android-support-v4 \
+								android-support-v7-recyclerview 										
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.recyclerview			
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := JAC-Appstore
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PACKAGE)
+
+

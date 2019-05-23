3d782bb313a53c84f44216dbc7599c281ab6758b
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-05-23 18:27:12 +0800

    Launcher 更换背景图，不以com.jancar开头的所有应用缩小图标显示

--- Launcher3/Android.mk
+++ Launcher3/Android.mk
@@ -6,8 +6,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
 LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res \
-                      $(LOCAL_PATH)/src/$(JAC_PRODUCT_UI_APX)/res \
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/$(JAC_PRODUCT_UI_APX)/res \
+                      $(LOCAL_PATH)/src/main/res \
                       $(LOCAL_PATH)/../wallpaperpick/src/main/res \
 	                  $(LOCAL_PATH)/../../../../../../prebuilts/sdk/current/support/v7/recyclerview/res
 LOCAL_JAVA_LIBRARIES := framework \
